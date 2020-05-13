3cafae7f16658c89509a48b4c87b7ffb89b8dd87
jcallinan <jeremy.callinan@gmail.com>
2020-05-13 17:03:50 -0400

    Update the-script.ps1

--- the-script.ps1
+++ the-script.ps1
@@ -1,3 +1,5 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
 choco install azure-data-studio
 choco install sql-server-management-studio
 # https://www.microsoft.com/en-us/download/details.aspx?id=54255
